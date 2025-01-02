b8cef7aa4275d1bf5cfe7dbaca9a8c92ad5f0134
HEROgold <martijnwieringa28@gmail.com>
2025-01-02 15:03:47 +0100

    typo

--- prototypes/roboport/logistical-roboports.lua
+++ prototypes/roboport/logistical-roboports.lua
@@ -9,19 +9,19 @@ local robot_storage_limit = math.max(robot_storage_limit, research_minimum)
 local material_storage_limit = math.max(material_storage_limit, research_minimum)
 local construction_area_limit = math.max(construction_area_limit, research_minimum)
 local logistic_area_limit = math.max(logistic_area_limit, research_minimum)
-local logisitcal_roboport_entity = table.deepcopy(base_roboport_entity)
-local logisitcal_roboport_item = table.deepcopy(base_roboport_item)
+local logistical_roboport_entity = table.deepcopy(base_roboport_entity)
+local logistical_roboport_item = table.deepcopy(base_roboport_item)
 
 
-logisitcal_roboport_item.name = RoboportLogistical
-logisitcal_roboport_entity.name = RoboportLogistical
+logistical_roboport_item.name = RoboportLogistical
+logistical_roboport_entity.name = RoboportLogistical
 
-logisitcal_roboport_item.place_result = logisitcal_roboport_entity.name
+logistical_roboport_item.place_result = logistical_roboport_entity.name
 -- logisitcal_roboport_item.hidden = false
 
-logisitcal_roboport_entity.minable.result = logisitcal_roboport_item.name
-logisitcal_roboport_entity.robot_slots_count = 10
-logisitcal_roboport_entity.material_slots_count = 10
+logistical_roboport_entity.minable.result = logistical_roboport_item.name
+logistical_roboport_entity.robot_slots_count = 10
+logistical_roboport_entity.material_slots_count = 10
 
 ---@type data.RecipePrototype
 local storage_roboport_recipe = {
@@ -34,14 +34,14 @@ local storage_roboport_recipe = {
         {type = "item", name ="steel-plate", amount = 100},
     },
     ---@type data.ItemProductPrototype[]
-    results = {{type = "item", name = logisitcal_roboport_entity.name, amount = 1},},
+    results = {{type = "item", name = logistical_roboport_entity.name, amount = 1},},
     category = "crafting",
     unlock_results = true,
 }
 local function add_storage_roboport()
-    data:extend({logisitcal_roboport_item})
+    data:extend({logistical_roboport_item})
     data:extend({storage_roboport_recipe})
-    data:extend({logisitcal_roboport_entity})
+    data:extend({logistical_roboport_entity})
 end
 
 local function add_stacking_storage_roboport()
@@ -49,8 +49,8 @@ local function add_stacking_storage_roboport()
         for l=0, logistic_area_limit do
             for r=0, robot_storage_limit do
                 for m=0, material_storage_limit do
-                    local roboport_item = table.deepcopy(logisitcal_roboport_item)
-                    local roboport_entity = table.deepcopy(logisitcal_roboport_entity)
+                    local roboport_item = table.deepcopy(logistical_roboport_item)
+                    local roboport_entity = table.deepcopy(logistical_roboport_entity)
 
                     local suffix = get_storage_suffix(c, l, r, m)
                     local name = combine{RoboportLogisticalLeveled .. suffix}
@@ -65,11 +65,11 @@ local function add_stacking_storage_roboport()
                     roboport_item.hidden = true
 
                     -- Level * base, + base
-                    rb = logisitcal_roboport_entity.robot_slots_count
-                    mb = logisitcal_roboport_entity.material_slots_count
-                    cb = logisitcal_roboport_entity.construction_radius
-                    lb = logisitcal_roboport_entity.logistics_radius
-                    ldb = logisitcal_roboport_entity.logistics_connection_distance or lb
+                    rb = logistical_roboport_entity.robot_slots_count
+                    mb = logistical_roboport_entity.material_slots_count
+                    cb = logistical_roboport_entity.construction_radius
+                    lb = logistical_roboport_entity.logistics_radius
+                    ldb = logistical_roboport_entity.logistics_connection_distance or lb
 
                     roboport_entity.robot_slots_count = (rb * r) + rb
                     roboport_entity.material_slots_count = (mb * m) + mb
